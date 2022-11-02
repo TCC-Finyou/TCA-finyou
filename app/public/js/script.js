e7da9032c1833c7e9f16c1fa7c72c6da4e4874dc
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2022-11-02 16:49:56 -0300

    animar menu

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -2,6 +2,11 @@ const menu = document.querySelector("[data-menu]")
 
 const menuItems = document.querySelector("[data-menu-items]")
 
+const menuSpans=document.querySelectorAll("[data-menu-span]")
+
 menu.addEventListener("click", () => {
     menuItems.classList.toggle("show")
+    menuSpans.forEach(menuSpan => {
+        menuSpan.classList.toggle("close")
+    })
 })
